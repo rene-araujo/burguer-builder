48b7b0f2ce252facb0698cec07a5d9ee3a99caf7
Unknown <renerogeraraujo@gmail.com>
2018-07-18 16:35:07 -0300

    aula 118

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -2,19 +2,33 @@ import * as React from 'react';
 import * as classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-// interface IProps {
-//     children: any;
-// }
+interface IProps {
+    ingredients: any;
+}
 
-const burger = () => {
+const burger = (props: IProps) => {
+
+    let transformedIngredients: JSX.Element[] | JSX.Element = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey]).fill('')].map((_, key) => {
+                return (<BurgerIngredient key={igKey + key} type={igKey} />);
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
-    )
+    );
 
 }
 
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -2,10 +2,20 @@ import * as React from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends React.Component {
+
+    public state = {
+        ingredients: {
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+            salad: 0
+        }
+    }
+
     public render() {
         return (
             <>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </>
         );
